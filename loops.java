bbcfa8bcef6d5a61aa4c1cdb797de0c8d91a544f
Blesson K Tomy <116932677+Blesson-Tomy@users.noreply.github.com>
2023-09-30 23:55:16 +0530

    Update with basic Java Programmes

---
+++ loops.java
@@ -0,0 +1,21 @@
+public class loops {
+    public static void main(String args[])
+    { int i=1;
+        do{
+            System.out.println("Welcome to Java Programming.");
+            break;
+        }while (i>0);
+
+        if(i==1)
+        {
+            System.out.println("These are the basics of C that I am bringing into Java. Java is an object-oriented language that focuses on portability and security. The use of the legendary intermediate byte code makes Java one of the most popular programming languages in the world.");
+        }
+        
+        for(i=0;i<2;i++)
+        {
+            System.out.println("In order for Java to work on any OS, a JDK needs to be installed. I just managed to install one. That is to say, the time of Java programming has begun! EXCITED");
+        }
+    }
+
+
+}
