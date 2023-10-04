74d25b5e1d6c13922d1c7a43214c71a5b997dcd3
Blesson K Tomy <116932677+Blesson-Tomy@users.noreply.github.com>
2023-10-04 12:27:37 +0530

    Uploaded Java files

---
+++ frequency.java
@@ -0,0 +1,33 @@
+package lab3;
+
+import java.util.Scanner;
+
+public class frequency {
+
+	public static void main(String[] args) {
+		
+		int i;
+		Scanner arr = new Scanner(System.in);
+		System.out.println("Enter the String:");
+		String s1 = arr.nextLine();
+		
+		System.out.println("Enter the character to check the frequency of:");
+		char s2 = arr.next().charAt(0);
+		
+		char[] ch = s1.toCharArray();
+		int x = s1.length();
+		int count=0;
+		
+		for(i=0;i<x;i++)
+		{
+			//if(s2==s1.charAt(i))
+			if(s2.equalsIgnoreCase(s1.charAt[i])==0)
+			{
+				count++;
+			}
+		}
+		System.out.println("There are "+count+" repetitions.");
+		
+	}
+
+}
